fc0a0464da22a2fdbe02178bd798a28b5a5bf0d7
Joe Desbonnet <jdesbonnet@gmail.com>
2025-04-02 21:02:07 +0100

    fix package name

--- src/main/java/ie/strix/gnss/ntrip/NTRIP.java
+++ src/main/java/ie/strix/gnss/ntrip/NTRIP.java
@@ -1,4 +1,4 @@
-package ie.strix.gnss;
+package ie.strix.gnss.ntrip;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,14 +8,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 
-
 //import lombok.extern.slf4j.Slf4j;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class NTRIP {
-
-
 
 	/**
 	 * Network TCP socket to NTRIP service.
@@ -26,15 +23,16 @@ public class NTRIP {
 	private String port;
 	private String auth;
 
-	public NTRIP (String host, String port, String auth) {
+	public NTRIP(String host, String port, String auth) {
 		this.host = host;
 		this.port = port;
 		this.auth = auth;
 	}
 
 	/**
-	 * Open NTRIP connection and read corrections message and forward to to GNSS device.
- 	 */
+	 * Open NTRIP connection and read corrections message and forward to to GNSS
+	 * device.
+	 */
 	private class NTRIPReader implements Runnable {
 
 		private Socket ntripSocket;
@@ -49,11 +47,11 @@ public class NTRIP {
 			try {
 				readLoop();
 			} catch (IOException e) {
-				log.error("NTRIP corrections stream read loop: ",e);
+				log.error("NTRIP corrections stream read loop: ", e);
 			}
 		}
 
-		public void setDisconnectTime (long t) {
+		public void setDisconnectTime(long t) {
 			this.disconnectTime = t;
 		}
 
@@ -63,61 +61,57 @@ public class NTRIP {
 			InputStream in = ntripSocket.getInputStream();
 			OutputStream out = ntripSocket.getOutputStream();
 
-			String request = "GET /NEAR-RTCM HTTP/1.1\r\n" +
-				"Host: " + host + ":" + port + "\r\n" +
-				"User-Agent: ie.strix.gnss.ntrip.NTRIPClient/0.1.0\r\n" +
-				"Authorization: " + auth + "\r\n" +
-				"Ntrip-Version: Ntrip/2.0\r\n" +
-				"Accept: */*\r\n" +
-				"Connection: close\r\n\r\n";
+			String request = "GET /NEAR-RTCM HTTP/1.1\r\n" + "Host: " + host + ":" + port + "\r\n"
+					+ "User-Agent: ie.strix.gnss.ntrip.NTRIPClient/0.1.0\r\n" + "Authorization: " + auth + "\r\n"
+					+ "Ntrip-Version: Ntrip/2.0\r\n" + "Accept: */*\r\n" + "Connection: close\r\n\r\n";
 			out.write(request.getBytes(StandardCharsets.UTF_8));
 			out.flush();
 
-            // Before passing binary RTCM messages to GNSS module, first read past the NTRIP response header
-            // which is terminated by two end-of-lines CR LF CR LF. We won't bother parsing any of this
-            // for the moment.
-            int endOfHeader = 0;
-            int n = 0;
-            while (endOfHeader != 0x0d0a0d0a) {
-                int c = in.read() & 0xff;
-                endOfHeader <<= 8;
-                endOfHeader |= c;
-                n++;
-                //log.info("reading NTRIP header byte: " + (char)c + " endOfHeader=" + String.format("%08x",endOfHeader));
-            }
-            log.info("end of NTRIP response header found");
+			// Before passing binary RTCM messages to GNSS module, first read past the NTRIP
+			// response header
+			// which is terminated by two end-of-lines CR LF CR LF. We won't bother parsing
+			// any of this
+			// for the moment.
+			int endOfHeader = 0;
+			int n = 0;
+			while (endOfHeader != 0x0d0a0d0a) {
+				int c = in.read() & 0xff;
+				endOfHeader <<= 8;
+				endOfHeader |= c;
+				n++;
+				// log.info("reading NTRIP header byte: " + (char)c + " endOfHeader=" +
+				// String.format("%08x",endOfHeader));
+			}
+			log.info("end of NTRIP response header found");
 
-            byte[] buf = new byte[1024];
-            int nbytes;
-            while ( (nbytes = in.read(buf)) >= 0) {
-                log.info("read " + nbytes + " from NTRIP stream");
+			byte[] buf = new byte[1024];
+			int nbytes;
+			while ((nbytes = in.read(buf)) >= 0) {
+				log.info("read " + nbytes + " from NTRIP stream");
 
-                StringBuilder sb = new StringBuilder();
-                int sumbytes = 0;
-                for (int i = 0; i < nbytes; i++) {
-                    sb.append(" " + String.format("%02x",buf[i]));
-                    sumbytes += buf[i];
-                }
-                log.info(sb.toString() + " sum=" + sumbytes);
+				StringBuilder sb = new StringBuilder();
+				int sumbytes = 0;
+				for (int i = 0; i < nbytes; i++) {
+					sb.append(" " + String.format("%02x", buf[i]));
+					sumbytes += buf[i];
+				}
+				log.info(sb.toString() + " sum=" + sumbytes);
 
-		/*
-                if (outToGnssDevice != null) {
-                    outToGnssDevice.write(buf,0,nbytes);
-                    outToGnssDevice.flush();
-                    log.info("writing " + nbytes + " from NTRIP stream to GNSS module");
-                    //mCurrentUpdate.correctionBytesWritten += nbytes;
-                }
-		*/
+				/*
+				 * if (outToGnssDevice != null) { outToGnssDevice.write(buf,0,nbytes);
+				 * outToGnssDevice.flush(); log.info("writing " + nbytes +
+				 * " from NTRIP stream to GNSS module"); //mCurrentUpdate.correctionBytesWritten
+				 * += nbytes; }
+				 */
 
-                if (disconnectTime > 0 &&  System.currentTimeMillis() >= disconnectTime) {
-                    log.info("NTRIP read stream disconnect time reached");
-                    ntripSocket.close();
-                    break;
-                }
+				if (disconnectTime > 0 && System.currentTimeMillis() >= disconnectTime) {
+					log.info("NTRIP read stream disconnect time reached");
+					ntripSocket.close();
+					break;
+				}
 
-            }
-            log.info("NTRIP read stream terminated");
-        }
-    }
+			}
+			log.info("NTRIP read stream terminated");
+		}
+	}
 }
-
