9b09db618b34878593b22af834db8ffe04674b5f
Joe Desbonnet <jdesbonnet@gmail.com>
2025-06-29 12:00:46 +0100

    code related to handling RTCM3 messages

---
+++ src/main/java/ie/strix/gnss/rtcm/RTCM3.java
@@ -0,0 +1,45 @@
+package ie.strix.gnss.rtcm;
+
+/**
+ * Related to RTCM v3 messages.
+ * 
+ *
+ */
+public class RTCM3 {
+	private static final int[] CRC24Q_TABLE = new int[256];
+	private static final int POLY = 0x1864CFB;
+
+	static {
+		for (int i = 0; i < 256; i++) {
+			int crc = i << 16;
+			for (int j = 0; j < 8; j++) {
+				if ((crc & 0x800000) != 0) {
+					crc = (crc << 1) ^ POLY;
+				} else {
+					crc <<= 1;
+				}
+			}
+			CRC24Q_TABLE[i] = crc & 0xFFFFFF;
+		}
+	}
+
+	/**
+	 * Compute the CRC-24Q over a byte array.
+	 *
+	 * @param data   Byte array containing the message (starting from preamble 0xD3)
+	 * @param length Number of bytes to include in the CRC (everything except final
+	 *               3-byte CRC)
+	 * @return 24-bit CRC as an int
+	 */
+	public static int computeCRC24Q(byte[] data, int length) {
+		int crc = 0;
+
+		for (int i = 0; i < length; i++) {
+			int b = data[i] & 0xFF;
+			int index = ((crc >> 16) ^ b) & 0xFF;
+			crc = ((crc << 8) ^ CRC24Q_TABLE[index]) & 0xFFFFFF;
+		}
+
+		return crc;
+	}
+}
