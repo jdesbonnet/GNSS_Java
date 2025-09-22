15867456a6a951882d07722e01abf2b814ed2f49
Joe Desbonnet <jdesbonnet@gmail.com>
2025-09-22 12:11:37 +0100

    add unit tests; fast lat/lng parser

--- src/test/java/ie/strix/gnss/nmea/TestParser.java
+++ src/test/java/ie/strix/gnss/nmea/TestParser.java
@@ -6,21 +6,25 @@ import org.junit.jupiter.api.Test;
 
 public class TestParser {
 
-
 	
 	@Test
 	public void testLongitudeParser() {
-		String nmeaLongitude = "00859.58729342,W";
-
-		double longitude = Util.parseNmeaLongitude(nmeaLongitude.toCharArray(), 0, 15);
-		assertEquals(-8.993121557, longitude, 0.000000001);
+		
+		double longitudePositive = Util.parseNmeaLongitude("00859.58729342,W".toCharArray(), 0, 15);
+		assertEquals(-8.993121557, longitudePositive, 0.000000001);
+		
+		double longitudeNegative = Util.parseNmeaLongitude("00859.58729342,W".toCharArray(), 0, 15);
+		assertEquals(-8.993121557, longitudeNegative, 0.000000001);
 	}
 	
-	public void testLatitudeParser() {
-		String nmeaLatitude = "5316.89755755,N";
+	@Test
+	public void testLatitudeParser() {		
 		
-		double latitude = Util.parseNmeaLatitude(nmeaLatitude.toCharArray(), 0, 14);
-		assertEquals(53.28162596, latitude, 0.000000001);
+		double latitudePositive = Util.parseNmeaLatitude("5316.89755755,N".toCharArray(), 0, 14);
+		assertEquals(53.28162596, latitudePositive, 0.000000001);
+		
+		double latitudeNegative = Util.parseNmeaLatitude("5316.89755755,S".toCharArray(), 0, 14);
+		assertEquals(-53.28162596, latitudeNegative, 0.000000001);		
 	}
 
 }
