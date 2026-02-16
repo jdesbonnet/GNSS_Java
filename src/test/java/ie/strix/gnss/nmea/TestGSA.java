bc0d19c451cb5cb3c1e7f724c526b401b9ea0166
Joe Desbonnet <jdesbonnet@gmail.com>
2026-02-16 16:11:22 +0000

    Handle malformed GSA satellite slots without NumberFormatException

--- src/test/java/ie/strix/gnss/nmea/TestGSA.java
+++ src/test/java/ie/strix/gnss/nmea/TestGSA.java
@@ -23,4 +23,19 @@ public class TestGSA {
         assertEquals(1.0, gsa.getHdop(), 1e-9);
         assertEquals(1.5, gsa.getVdop(), 1e-9);
     }
+    @DisplayName("GSA tolerates missing final satellite slots before DOP fields")
+    @Test
+    public void testGSAMissingSatelliteSlots() throws ChecksumFailException {
+        GSA gsa = new GSA("$GPGSA,A,3,04,05,09,12,24,25,29,31,,,1.8,1.0,1.5*3D");
+
+        assertEquals(12, gsa.getSatellitePrns().length);
+        assertNull(gsa.getSatellitePrns()[8]);
+        assertNull(gsa.getSatellitePrns()[9]);
+        assertNull(gsa.getSatellitePrns()[10]);
+        assertNull(gsa.getSatellitePrns()[11]);
+        assertEquals(1.8, gsa.getPdop(), 1e-9);
+        assertEquals(1.0, gsa.getHdop(), 1e-9);
+        assertEquals(1.5, gsa.getVdop(), 1e-9);
+    }
+
 }
